dfee89e487ad3a912b41eacf53d0907e64a5ed9d
Matt F <matt.freedman@jointheleague.org>
2018-06-12 17:49:22 -0700

    Made SeaCreature instructions show up

--- src/SeaCreature.java
+++ src/SeaCreature.java
@@ -3,10 +3,8 @@
  *    Level 1
  */
 
-/* 
- * 1. In a Runner class, make a SeaCreature called “Spongebob”. Use the methods below to make him eat, and laugh.
- * 2. Make Patrick and Squidward and print their name, have them eat, and make them laugh.
- */
+// 1. In a Runner class, make a SeaCreature called “Spongebob”. Use the methods below to make him eat, and laugh.
+// 2. Make Patrick and Squidward and print their name, have them eat, and make them laugh.
 
 
 import javax.swing.JApplet;
