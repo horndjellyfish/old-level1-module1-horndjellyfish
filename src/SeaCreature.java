e2b6d7110c0d21c9804e7fe80e65c66775ca5cfb
Matt F <matt.freedman@jointheleague.org>
2018-06-26 19:15:03 -0700

    Fixed SeaCreature Instructions for good?

--- src/SeaCreature.java
+++ src/SeaCreature.java
@@ -3,14 +3,14 @@
  *    Level 1
  */
 
-// 1. In a Runner class, make a SeaCreature called “Spongebob”. Use the methods below to make him eat, and laugh.
-// 2. Make Patrick and Squidward and print their name, have them eat, and make them laugh.
-
-
 import javax.swing.JApplet;
 import java.applet.AudioClip;
 
 public class SeaCreature {
+	
+	// 1. In a Runner class, make a SeaCreature called “Spongebob”. Use the methods below to make him eat, and laugh.
+	// 2. Make Patrick and Squidward and print their name, have them eat, and make them laugh.
+
 
 	private String name;
 
